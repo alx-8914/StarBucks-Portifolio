aa4d47e1d58afcb7ecee7bf7d94debab27b7447a
seu-username <alxtronic.alex2011@gmail.com>
2024-04-21 22:40:28 -0300

    Alteração no Projeto index.html,adicionado imagem e video, javascripts acrescetado

---
+++ scripts.js
@@ -0,0 +1,22 @@
+
+    let circulo = document.querySelector(".circulo")
+    let imgCopo = document.querySelector(".starbucks")
+    let verde = document.querySelector(".verde")
+    let amarelo = document.querySelector(".amarelo")
+    let rosa = document.querySelector(".rosa")
+
+
+    verde.addEventListener("click", () => {
+        imgCopo.src = "./img1.png"
+        circulo.style.background = "#017143"
+    })
+    amarelo.addEventListener("click", () => {
+        imgCopo.src = "./img2.png"
+        circulo.style.background = "#eb7495"
+    })
+    rosa.addEventListener("click", () => {
+        imgCopo.src = "./img3.png"
+        circulo.style.background = "#d752b1"
+
+    })
+
